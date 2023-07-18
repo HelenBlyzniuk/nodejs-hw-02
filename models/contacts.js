c0a69a0bf24561810509676146284a3f728f745c
HelenBlyzniuk <115355592+HelenBlyzniuk@users.noreply.github.com>
2023-07-18 19:06:31 +0300

    Update contacts.js

--- models/contacts.js
+++ models/contacts.js
@@ -28,7 +28,7 @@ const removeContact = async (contactId) => {
     return result;
 }
 
-const addContact = async (name,email,phone) => {
+const addContact = async ({name,email,phone}) => {
   const contacts = await listContacts();
   const newContact = { id: nanoid(), name, email, phone };
   contacts.push(newContact);
