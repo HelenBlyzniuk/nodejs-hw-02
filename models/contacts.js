4de633544cf53a595ba5fe595ea73c5ddf6376fb
HelenBlyzniuk <115355592+HelenBlyzniuk@users.noreply.github.com>
2023-07-19 14:19:05 +0300

    +

--- models/contacts.js
+++ models/contacts.js
@@ -43,6 +43,7 @@ if(index===-1){
   return null;
 }
 contacts[index]={contactId,body}
+console.log(contacts[index])
 await fs.writeFile(contactsPath,JSON.stringify(contacts,null,2))
 return contacts [index]
 }
